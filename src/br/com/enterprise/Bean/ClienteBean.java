f8450b16c5d7820ac1b682fccf8caed5f3842b79
Jessé Sampaio <jessesampaio.ti@gmail.com>
2018-02-24 22:41:21 -0300

    Implementação do ClienteBean
    Implementação do ClienteBean;
    
    Bean: Todas as informações trafegadas do JSF funcionanm através de
    strings,
    e toda ligação feita do formulário precisa ser ligado a um Bean.
    Precisa de uma classe Bean, um Bean para cada Entidade.
    Todas as informações são feitas através de Strings.
    
    Foram criadas as páginas: sucesso.xhtml e erro.xhtml;
    
    Criação do banco de dados jsf_crud no MySQL.
    
    Teste de cadastro e persistência no banco de dados com sucesso.

--- src/br/com/enterprise/Bean/ClienteBean.java
+++ src/br/com/enterprise/Bean/ClienteBean.java
@@ -1,9 +1,27 @@
 package br.com.enterprise.Bean;
 
-public class ClienteBean {
+import javax.faces.bean.ManagedBean;
 
-	public ClienteBean() {
+import br.com.enterprise.DAO.ClienteDAO;
+import br.com.enterprise.VO.ClienteVO;
+
+
+@ManagedBean(name="cliBean")
+public class ClienteBean extends ClienteVO{
+	
+	public String addUser(){
+		String retorno = "erro";
 		
-	}
+		try {
+			ClienteDAO dao = new ClienteDAO();
+			if(dao.insert(this)){
+				retorno = "sucesso";
+			}							
+		} catch (Exception ex) {
+
+		}
+		
+		return retorno;
+	}	 
 
 }

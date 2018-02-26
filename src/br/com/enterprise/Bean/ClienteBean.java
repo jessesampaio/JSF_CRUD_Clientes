33b5345f5c69319223ad997a502d0b97d9faa5dd
Jessé Sampaio <jessesampaio.ti@gmail.com>
2018-02-25 22:56:34 -0300

    Implementação do DataTable de Clientes
    Implementação do DataTable de Clientes.

--- src/br/com/enterprise/Bean/ClienteBean.java
+++ src/br/com/enterprise/Bean/ClienteBean.java
@@ -1,6 +1,11 @@
 package br.com.enterprise.Bean;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 import br.com.enterprise.DAO.ClienteDAO;
 import br.com.enterprise.VO.ClienteVO;
@@ -9,12 +14,17 @@ import br.com.enterprise.VO.ClienteVO;
 @ManagedBean(name="cliBean")
 public class ClienteBean extends ClienteVO{
 	
+	private ClienteVO clienteVO = new ClienteVO();
+	private DataModel<ClienteVO> clientes;
+	
+	
+	
 	public String addUser(){
 		String retorno = "erro";
 		
 		try {
 			ClienteDAO dao = new ClienteDAO();
-			if(dao.insert(this)){
+			if(dao.insert(clienteVO)){
 				retorno = "sucesso";
 			}							
 		} catch (Exception ex) {
@@ -22,6 +32,30 @@ public class ClienteBean extends ClienteVO{
 		}
 		
 		return retorno;
-	}	 
+	}
+	
+	//Getters and Setters
+	public ClienteVO getClienteVO() {
+		return clienteVO;
+	}
+	public void setClienteVO(ClienteVO clienteVO) {
+		this.clienteVO = clienteVO;
+	}
+	public DataModel<ClienteVO> getClientes() {
+		
+		ClienteDAO dao = new ClienteDAO();
+		
+		try {
+			List<ClienteVO> lista = dao.getAll();
+			clientes = new ListDataModel<ClienteVO>(lista);
+		} catch (Exception e) {
+			
+		}
+		
+		return clientes;
+	}
+	public void setClientes(DataModel<ClienteVO> clientes) {
+		this.clientes = clientes;
+	}
 
 }
